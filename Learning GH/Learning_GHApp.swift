8f92052fb692ce66bc2e9576377cf212c744f0d7
Petr Roubal <petr@machal.net>
2022-11-27 13:35:24 +0100

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Petr Roubal on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
